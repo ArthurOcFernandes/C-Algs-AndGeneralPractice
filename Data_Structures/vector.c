14308a4af0fe121e26bc503455a61e74e189acce
Arthur <arthurocsilva@gmail.com>
2022-03-29 14:38:04 -0300

    init function created

--- Data_Structures/vector.c
+++ Data_Structures/vector.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+typedef struct vector
+{
+    int *arr, size, elements;
+} vector;
 
-    
+void printVector(vector *v);
+void initializeVector(vector *v);
+
+int main()
+{
+    vector v;
+    initializeVector(&v);
+
+    for(int i = 0; i < v.size; i++){
+        v.arr[i] = i;
+    }
+
+    printVector(&v);
 
     return 0;
+}
+
+void initializeVector(vector* v){
+    v->size = 20;
+    v->arr =  (int*)(malloc(sizeof(int) *  20));
+    v->elements = 0;
+}
+
+void printVector(vector *v)
+{
+    for (int i = 0; i < v->size; i++)
+    {
+        printf("%d ", v->arr[i]);
+    }
+    printf("\n");
 }
