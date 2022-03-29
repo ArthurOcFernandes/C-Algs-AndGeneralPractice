a5828fac11b88a9c0236f8b94be549245b5431ca
Arthur <arthurocsilva@gmail.com>
2022-03-29 15:28:33 -0300

    vector now reallocates dinamically

--- Data_Structures/vector.c
+++ Data_Structures/vector.c
@@ -8,30 +8,59 @@ typedef struct vector
 
 void printVector(vector *v);
 void initializeVector(vector *v);
+void push(vector *v, int value);
 
 int main()
 {
     vector v;
     initializeVector(&v);
 
-    for(int i = 0; i < v.size; i++){
-        v.arr[i] = i;
+    for (int i = 0; i < v.size; i++)
+    {
+        push(&v, i);
     }
-
+    push(&v, 20);
+    push(&v, 22);
+    push(&v, 50);
+    push(&v, 2);
+    printf("%d\n", v.elements);
     printVector(&v);
-
     return 0;
 }
 
-void initializeVector(vector* v){
+void copy(int vet[], int vet2[], int size1){
+
+    for(int i = 0; i < size1; i++){
+        vet2[i] = vet[i];
+    }
+
+}
+
+void push(vector *v, int value)
+{
+
+    if (v->elements <= v->size)
+    {
+        v->arr[v->elements++] = value;
+    }
+    else
+    {   
+        v->arr = realloc(v->arr, 2 * v->size * sizeof(int));
+        v->size *= 2;
+        push(v, value);
+    }
+}
+
+void initializeVector(vector *v)
+{
     v->size = 20;
-    v->arr =  (int*)(malloc(sizeof(int) *  20));
+    v->arr = (int *)(malloc(sizeof(int) * 20));
     v->elements = 0;
 }
 
 void printVector(vector *v)
 {
-    for (int i = 0; i < v->size; i++)
+    for (int i = 0; i < v->elements; i++)
     {
         printf("%d ", v->arr[i]);
     }
